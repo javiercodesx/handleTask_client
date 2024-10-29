3e7274b29256e1c95ba3746a5ac192d790cdcbcb
javxdev <javxdev@gmail.com>
2024-10-29 11:59:46 -0300

    add Router, DashboardPage, AppLayout

---
+++ src/layouts/AppLayout.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom"
+
+export default function AppLayout() {
+  return (
+    <>
+        <div>
+            AppLayout
+        </div>
+        
+        <Outlet/>
+    </>
+  )
+}
---
+++ src/router.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AppLayout from "./layouts/AppLayout";
+import DashboardPage from "./pages/DashboardPage";
+
+export const Router = () => {
+    
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route
+                    element={ <AppLayout/> }
+                >
+                    <Route path="/" element={ <DashboardPage/> } index/>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
+}
