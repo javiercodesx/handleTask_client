69b67f23a67381c474d74a74a48caf8b27a425ea
javxdev <javxdev@gmail.com>
2024-10-30 11:30:36 -0300

    add header & footer

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,13 +1,28 @@
 import { Outlet } from "react-router-dom"
+import Logo from "../components/Logo"
 
 export default function AppLayout() {
   return (
     <>
-        <div>
-            AppLayout
-        </div>
+        <header className="bg-gray-800 py-5">
+            <div className="max-w-screen-2xl mx-auto flex flex-col lg:flex-row justify-between items-center">
+              <div className="w-64">
+                <Logo/>
+              </div>
+              
+              <nav></nav>
+            </div>
+        </header>
         
-        <Outlet/>
+        <section className="max-w-screen-2xl mx-auto mt-10 p-5">
+          <Outlet/>
+        </section>
+
+        <footer className="py-5">
+          <p className="text-center">
+            All rights reserved {new Date().getFullYear()}
+          </p>
+        </footer>
     </>
   )
 }
