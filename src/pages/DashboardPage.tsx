fb517087d3a9b617c8491e4d2e57cbed130c2264
javxdev <javxdev@gmail.com>
2024-11-05 20:56:57 -0300

    add CreateProjectPage

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,6 +1,17 @@
+import { Link } from "react-router-dom"
 
 export default function DashboardPage() {
   return (
-    <div>DashboardPage</div>
+    <>
+      <h1 className="text-5xl font-black">My projects</h1>
+      <p className="text-2xl font-light text-gray-500 mt-5">Handle your projects</p>
+    
+      <nav className="my-8">
+        <Link
+          className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
+          to='/projects/create'
+        >New project</Link>
+      </nav>
+    </>
   )
 }
--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AppLayout from "@/layouts/AppLayout";
 import DashboardPage from "@/pages/DashboardPage";
+import CreateProjectPage from "./pages/projects/CreateProjectPage";
 
 export const Router = () => {
     
@@ -11,6 +12,7 @@ export const Router = () => {
                     element={ <AppLayout/> }
                 >
                     <Route path="/" element={ <DashboardPage/> } index/>
+                    <Route path="/projects/create" element={ <CreateProjectPage/> } index/>
                 </Route>
             </Routes>
         </BrowserRouter>
