e1d1e88f6b3bc9f7456c5d260aef546bb40b6b96
javxdev <javxdev@gmail.com>
2024-11-07 18:10:31 -0300

    add create project form

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -11,19 +11,35 @@ export default function DashboardPage() {
 
   const { register, handleSubmit, formState: { errors } } = useForm({defaultValues: initialValues})
 
+  const handleForm = (data) => {
+    console.log(data)
+  }
+
   return (
     <>
-      <h1 className="text-5xl font-black">My projects</h1>
-      <p className="text-2xl font-light text-gray-500 mt-5">Handle your projects</p>
-    
-      <nav className="my-8">
-        <Link
-          className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
-          to='/projects/create'
-        >New project</Link>
-      </nav>
-
+      <div className="max-w-3xl mx-auto">
+        <h1 className="text-5xl font-black">My projects</h1>
+        <p className="text-2xl font-light text-gray-500 mt-5">Handle your projects</p>
       
+        <nav className="my-8">
+          <Link
+            className="bg-purple-600 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
+            to='/projects/create'
+          >New project</Link>
+        </nav>
+
+        <form
+          className=" mt-10 bg-white shadow-md p-10 rounded-md"
+          onSubmit={handleSubmit(handleForm)}
+          noValidate
+        >
+          <input 
+            type="submit" 
+            value="Create project"
+            className="bg-fuchsia-700 hover:bg-fuchsia-600 w-full p-3 text-white uppercase font-bold cursor-pointer transition-colors"
+          />
+        </form>
+      </div>
     </>
   )
 }
