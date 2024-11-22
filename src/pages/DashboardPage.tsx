6791d3862acf8699dcf991297d873f7aeb25e376
javxdev <javxdev@gmail.com>
2024-11-22 18:11:56 -0300

    update layout styles

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -4,18 +4,15 @@ export default function DashboardPage() {
 
   return (
     <>
-      <div className="max-w-3xl mx-auto">
-        <h1 className="text-4xl font-black">My projects</h1>
-        <p className="text-xl font-light text-gray-500 mt-5">Handle your projects</p>
-      
-        <nav className="my-8">
-          <Link
-            className="bg-gray-950 text-gray-300 border border-gray-300 hover:text-white px-4 py-2 font-semibold cursor-pointer transition-colors"
-            to='/projects/create'
-          >New project</Link>
-        </nav>
-
-      </div>
+      <h1 className="text-4xl font-black">My projects</h1>
+      <p className="text-xl font-light text-gray-500 mt-5">Handle your projects</p>
+    
+      <nav className="my-8">
+        <Link
+          className="bg-gray-950 text-gray-300 border border-gray-300 hover:text-white px-4 py-2 font-semibold cursor-pointer transition-colors"
+          to='/projects/create'
+        >New project</Link>
+      </nav>
     </>
   )
 }
