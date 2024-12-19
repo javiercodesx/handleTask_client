4159cb96345e415e730c1217c1228068fd600f70
javxdev <javxdev@gmail.com>
2024-12-19 14:29:27 -0300

    add show dialog

--- src/components/tasks/AddTaskModal.tsx
+++ src/components/tasks/AddTaskModal.tsx
@@ -1,13 +1,19 @@
 import { Fragment } from 'react';
-import { Dialog, Transition } from '@headlessui/react';
+import { Dialog, DialogPanel, DialogTitle, Transition, TransitionChild } from '@headlessui/react';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export default function AddTaskModal() {
+    const navigate = useNavigate()
+    const location = useLocation()
+    const queryParams = new URLSearchParams(location.search)
+    const modalTask = queryParams.get('newTask')
+    const show = modalTask ? true : false
 
     return (
         <>
-            <Transition appear show={true} as={Fragment}>
-                <Dialog as="div" className="relative z-10" onClose={() => { }}>
-                    <Transition.Child
+            <Transition appear show={show} as={Fragment}>
+                <Dialog as="div" className="relative z-10" onClose={() => navigate('', {replace: true})}>
+                    <TransitionChild
                         as={Fragment}
                         enter="ease-out duration-300"
                         enterFrom="opacity-0"
@@ -17,11 +23,11 @@ export default function AddTaskModal() {
                         leaveTo="opacity-0"
                     >
                         <div className="fixed inset-0 bg-black/60" />
-                    </Transition.Child>
+                    </TransitionChild>
 
                     <div className="fixed inset-0 overflow-y-auto">
                         <div className="flex min-h-full items-center justify-center p-4 text-center">
-                            <Transition.Child
+                            <TransitionChild
                                 as={Fragment}
                                 enter="ease-out duration-300"
                                 enterFrom="opacity-0 scale-95"
@@ -30,20 +36,20 @@ export default function AddTaskModal() {
                                 leaveFrom="opacity-100 scale-100"
                                 leaveTo="opacity-0 scale-95"
                             >
-                                <Dialog.Panel className="w-full max-w-4xl transform overflow-hidden rounded-2xl bg-white text-left align-middle shadow-xl transition-all p-16">
-                                    <Dialog.Title
+                                <DialogPanel className="w-full max-w-4xl transform overflow-hidden rounded-2xl bg-white text-left align-middle shadow-xl transition-all p-16">
+                                    <DialogTitle
                                         as="h3"
                                         className="font-black text-4xl  my-5"
                                     >
                                         Nueva Tarea
-                                    </Dialog.Title>
+                                    </DialogTitle>
 
                                     <p className="text-xl font-bold">Llena el formulario y crea  {''}
                                         <span className="text-fuchsia-600">una tarea</span>
                                     </p>
 
-                                </Dialog.Panel>
-                            </Transition.Child>
+                                </DialogPanel>
+                            </TransitionChild>
                         </div>
                     </div>
                 </Dialog>
