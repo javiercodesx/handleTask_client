5198d4743379f73c8c7ca15ae3e506c1af172862
javxdev <javxdev@gmail.com>
2024-12-26 19:16:14 -0300

    add task status names

--- src/components/tasks/TaskList.tsx
+++ src/components/tasks/TaskList.tsx
@@ -17,6 +17,14 @@ const initialStatusGroups : GroupedTasks = {
     completed: []
 }
 
+const statusNames : {[key: string]: string} = {
+    pending: 'Pending',
+    onHold: 'On hold',
+    inProgress: 'In progress',
+    underReview: 'Under review',
+    completed: 'Completed' 
+}
+
 export default function TaskList({tasks} : TaskListProps) {
 
     const groupedTasks = tasks.reduce((acc, task) => {
@@ -32,6 +40,11 @@ export default function TaskList({tasks} : TaskListProps) {
             <div className='flex gap-5 overflow-x-scroll 2xl:overflow-auto pb-32'>
                 {Object.entries(groupedTasks).map(([status, tasks]) => (
                     <div key={status} className='min-w-[300px] 2xl:min-w-0 2xl:w-1/5'>
+
+                        <h3 
+                            className={`capitalize text-xl font-light border border-x-slate-300 bg-white  p-3 border-t-8`}
+                        >{statusNames[status]}</h3>
+
                         <ul className='mt-5 space-y-5'>
                             {tasks.length === 0 ? (
                                 <li className="text-gray-500 text-center pt-3">No Hay tareas</li>
@@ -39,6 +52,7 @@ export default function TaskList({tasks} : TaskListProps) {
                                 tasks.map(task => <TaskCard key={task._id} task={task} />)
                             )}
                         </ul>
+
                     </div>
                 ))}
             </div>
