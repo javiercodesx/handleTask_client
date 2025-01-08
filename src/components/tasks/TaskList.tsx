30e86144b2cc9828e74d100b6931ad7ed4e5a169
javxdev <javxdev@gmail.com>
2025-01-08 09:27:53 -0300

    add TaskList, AppLayout styles

--- src/components/tasks/TaskList.tsx
+++ src/components/tasks/TaskList.tsx
@@ -45,12 +45,11 @@ export default function TaskList({tasks} : TaskListProps) {
         <>
             <h2 className="text-3xl font-bold my-10">Tasks</h2>
 
-            <div className='flex gap-5 overflow-x-scroll 2xl:overflow-auto pb-32'>
+            <div className='flex flex-col md:flex-row gap-5 2xl:overflow-auto pb-10 md:pb-32 '>
                 {Object.entries(groupedTasks).map(([status, tasks]) => (
-                    <div key={status} className='min-w-[300px] 2xl:min-w-0 2xl:w-1/5'>
+                    <div key={status} className='min-w-[300px] 2xl:min-w-0 2xl:w-1/5 '>
 
-                        <h3 
-                            className={`capitalize border border-x-slate-300 bg-white p-3 font-bold border-t-8 ${statusStyles[status]}`}
+                        <h3 className={`capitalize border border-x-slate-300 bg-white p-3 font-bold border-t-8 ${statusStyles[status]}`}
                         >{statusNames[status]}</h3>
 
                         <ul className='mt-5 space-y-5'>
--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -7,9 +7,9 @@ import NavMenu from "@/components/NavMenu"
 export default function AppLayout() {
   return (
     <>
-        <header className="bg-gray-900 py-5">
-            <div className="max-w-screen-2xl mx-auto flex flex-col lg:flex-row justify-between items-center">
-              <div className="w-64">
+        <header className="bg-gray-900 py-5 px-3">
+            <div className="max-w-screen-2xl mx-auto flex flex-col lg:flex-row justify-between items-center gap-2">
+              <div className="w-52 md:w-64">
                 <Link to={'/'}>
                   <Logo/>
                 </Link>
