31fceb45d2a7b6528eab7b1a7a6571b0ff24ca9f
javxdev <javxdev@gmail.com>
2025-01-20 13:05:35 -0300

    adding AuthLayout, LoginPage

--- src/router.tsx
+++ src/router.tsx
@@ -4,6 +4,8 @@ import DashboardPage from "@/pages/DashboardPage";
 import CreateProjectPage from "./pages/projects/CreateProjectPage";
 import EditProjectPage from "./pages/projects/EditProjectPage";
 import ProjectDetailsPage from "./pages/projects/ProjectDetailsPage";
+import AuthLayout from "./layouts/AuthLayout";
+import LoginPage from "./pages/auth/LoginPage";
 
 export const Router = () => {
     
@@ -17,6 +19,12 @@ export const Router = () => {
                     <Route path="/projects/create" element={ <CreateProjectPage/> }/>
                     <Route path="/projects/:projectId" element={ <ProjectDetailsPage/> }/>
                     <Route path="/projects/:projectId/edit" element={ <EditProjectPage/> }/>
+                </Route>
+
+                <Route
+                    element={ <AuthLayout/> }
+                >
+                    <Route path="/auth/login" element={ <LoginPage/> } />
                 </Route>
             </Routes>
         </BrowserRouter>
