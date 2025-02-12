f69406964d41cee5c86ba80e74dbdd763b81b8d4
javxdev <javxdev@gmail.com>
2025-02-12 12:44:13 -0300

    updating NavMenu styles

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -21,18 +21,18 @@ export default function NavMenu() {
         leaveTo="opacity-0 translate-y-1"
       >
         <PopoverPanel className="absolute left-1/2 z-10 mt-5 flex w-screen lg:max-w-min -translate-x-1/2 lg:-translate-x-48">
-          <div className="w-full lg:w-56 shrink rounded-xl bg-gray-300 p-4 text-sm font-semibold leading-6 text-gray-900 shadow-lg ring-1 ring-gray-900/5">
-            <p className='text-center text-gray-800 my-4'>Hello User!</p>
+          <div className="w-full lg:w-56 shrink rounded-xl bg-gray-900 p-4 text-sm font-semibold leading-6 text-gray-900 shadow-lg ring-1 ring-gray-900/5">
+            <p className='text-center text-gray-200 my-4'>Hello User!</p>
             <Link
               to='/profile'
-              className='block text-center text-gray-500 p-2 hover:text-gray-950'
+              className='block text-center text-gray-300 p-2 hover:text-white'
             >My Profile</Link>
             <Link
               to='/'
-              className='block text-center text-gray-500 p-2 hover:text-gray-950'
+              className='block text-center text-gray-300 p-2 hover:text-white'
             >My projects</Link>
             <button
-              className='block mx-auto text-gray-500 p-2 hover:text-red-600'
+              className='block mx-auto text-gray-300 p-2 hover:text-red-600'
               type='button'
               onClick={() => { }}
             >
