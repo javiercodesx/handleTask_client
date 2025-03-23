7e6c6228c977e820edc17e244f31015a0dae39f4
Javier <javxdev@gmail.com>
2025-03-23 18:52:06 -0300

    adding the jwt in localstorage

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
@@ -8,10 +7,8 @@ import { Router } from './router'
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
     <QueryClientProvider client={queryClient}>
       <Router/>
       <ReactQueryDevtools/>
     </QueryClientProvider>
-  </StrictMode>,
 )
