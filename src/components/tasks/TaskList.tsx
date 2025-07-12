3946489c1c5fb8958938532df89b21d0be18bd86
javier <javierbk9@gmail.com>
2025-07-12 15:11:39 -0300

    update TaskList styles

--- src/components/tasks/TaskList.tsx
+++ src/components/tasks/TaskList.tsx
@@ -9,7 +9,7 @@ type GroupedTasks = {
     [key: string]: Task[]
 }
 
-const initialStatusGroups : GroupedTasks = {
+const initialStatusGroups: GroupedTasks = {
     pending: [],
     onHold: [],
     inProgress: [],
@@ -17,37 +17,39 @@ const initialStatusGroups : GroupedTasks = {
     completed: []
 }
 
-export const statusNames : {[key: string]: string} = {
+export const statusNames: { [key: string]: string } = {
     pending: 'Pending',
     onHold: 'On hold',
     inProgress: 'In progress',
     underReview: 'Under review',
-    completed: 'Completed' 
+    completed: 'Completed'
 }
 
-const statusStyles : {[key: string]: string} = {
+const statusStyles: { [key: string]: string } = {
     pending: 'border-t-slate-500',
     onHold: 'border-t-red-500',
     inProgress: 'border-t-blue-500',
     underReview: 'border-t-amber-500',
-    completed: 'border-t-emerald-500' 
+    completed: 'border-t-emerald-500'
 }
 
-export default function TaskList({tasks} : TaskListProps) {
+export default function TaskList({ tasks }: TaskListProps) {
 
-    const groupedTasks = tasks.reduce((acc, task) => {
+    const groupedTasks : GroupedTasks = tasks.reduce((acc, task) => {
         let currentGroup = acc[task.status] ? [...acc[task.status]] : [];
         currentGroup = [...currentGroup, task]
         return { ...acc, [task.status]: currentGroup };
     }, initialStatusGroups);
-  
+
     return (
         <>
             <h2 className="text-3xl font-bold my-10">Tasks</h2>
 
-            <div className='flex flex-col md:flex-row gap-5 2xl:overflow-auto pb-10 md:pb-32 '>
+            <div className="grid gap-5 pb-10 md:pb-32 2xl:overflow-auto
+                grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+
                 {Object.entries(groupedTasks).map(([status, tasks]) => (
-                    <div key={status} className='min-w-[300px] 2xl:min-w-0 2xl:w-1/5 '>
+                    <div key={status} className='min-w-fit'>
 
                         <h3 className={`capitalize border border-x-slate-300 bg-white p-3 font-bold border-t-8 ${statusStyles[status]}`}
                         >{statusNames[status]}</h3>
@@ -64,5 +66,5 @@ export default function TaskList({tasks} : TaskListProps) {
                 ))}
             </div>
         </>
-  )
+    )
 }
